232ab8818caba83499b1d80c19f00bdf3622753d
Artem Khvostyk <5hv5art@gmail.com>
2021-10-17 18:13:06 +0300

    feat: add caption, change slider behaviour

--- cssMemSlider/assets/script.js
+++ cssMemSlider/assets/script.js
@@ -1,39 +1,105 @@
 const slider = document.querySelector('.slider');
-const sliderPaginationBoxes = slider.querySelectorAll(
-  '.slider__pagination-box'
-);
-const sliderTrack = slider.querySelector('.slider__track');
 const sliderSlides = [...slider.querySelectorAll('.slider__slide')];
 const sliderContainer = slider.querySelector('.slider__container');
+const sliderPagination = slider.querySelector('.slider__pagination');
 const slideWidth = sliderContainer.offsetWidth;
-let currentSlide = 0;
+const sliderCaption = slider.querySelector('.slider__slide-caption');
+let curSlideIndex = 0;
+let prevSlideIndex = 0;
+let lastCaption = null;
+const sliderPaginationBoxes = Array.from(Array(sliderSlides.length), (_, idx) =>
+  createPaginationBox(idx)
+);
+const slideCopies = [];
 
-sliderPaginationBoxes.forEach((box, idx) => {
-  box.addEventListener('click', () => setCurrentSlide(idx));
-});
+function createPaginationBox(idx) {
+  const paginationBox = document.createElement('div');
+  paginationBox.classList.add('slider__pagination-box');
+  paginationBox.classList.toggle(
+    'slider__pagination-box_current',
+    idx === curSlideIndex
+  );
+  paginationBox.innerHTML = '<div class="slider__pagination-bullet"></div>';
+  sliderPagination.appendChild(paginationBox);
+  paginationBox.addEventListener('click', () => setCurSlideIndex(idx));
+  return paginationBox;
+}
 
-function setCurrentSlide(idx) {
-  currentSlide = idx;
+function createCaption(text) {
+  const caption = document.createElement('p');
+  caption.innerText = text;
+  sliderCaption.appendChild(caption);
+  return caption;
+}
+
+function setCurSlideIndex(idx) {
+  prevSlideIndex = curSlideIndex;
+  curSlideIndex = idx;
   slideAndPaginate();
 }
 
 function paginate() {
-  sliderPaginationBoxes.forEach((box) => {
-    box.classList.remove('slider__pagination-box_current');
+  sliderPaginationBoxes.forEach((box, idx) => {
+    box.classList.toggle(
+      'slider__pagination-box_current',
+      curSlideIndex === idx
+    );
   });
-  sliderPaginationBoxes[currentSlide].classList.add(
-    'slider__pagination-box_current'
-  );
 }
 
 function slide() {
-  sliderTrack.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-  // sliderSlides.slice(0, currentSlide - 3).forEach((slide) => {
-  //   slide.style.right = '-100%';
-  // });
-  // sliderSlides.slice(currentSlide).forEach((slide) => {
-  //   slide.style.left = '-100%';
-  // });
+  slideCopies.forEach((node) => node.remove());
+  const curSlide = sliderSlides[curSlideIndex].cloneNode(true);
+  const curCaption = createCaption(curSlide.dataset.caption);
+
+  if (curSlideIndex === prevSlideIndex) {
+    curSlide.style.display = 'block';
+    curSlide.style.left = 0;
+    sliderContainer.appendChild(curSlide);
+    sliderCaption.appendChild(curCaption);
+    slideCopies.push(curSlide);
+    return;
+  }
+
+  const prevSlide = sliderSlides[prevSlideIndex].cloneNode(true);
+  const side = curSlideIndex > prevSlideIndex ? 'left' : 'right';
+  const isLeft = side === 'left';
+  curSlide.style.display = 'block';
+  curSlide.style.left = isLeft ? '100%' : '-100%';
+  curSlide.style.zIndex = 1;
+  prevSlide.style.display = 'block';
+  prevSlide.style.left = 0;
+
+  sliderContainer.append(prevSlide, curSlide);
+  slideCopies.push(curSlide, prevSlide);
+
+  prevCaption = createCaption(prevSlide.dataset.caption);
+  sliderCaption.innerHTML = '';
+  sliderCaption.append(prevCaption, curCaption);
+  captionWidth = prevCaption.offsetWidth;
+  curCaption.style.left = isLeft
+    ? `${curCaption.offsetWidth}px`
+    : `-${captionWidth}px`;
+
+  curCaption?.animate(
+    { left: '0' },
+    { duration: 500, easing: 'ease', fill: 'forwards' }
+  );
+  prevCaption?.animate(
+    {
+      left: isLeft ? `-${captionWidth}px` : `${curCaption.offsetWidth}px`,
+      opacity: 0,
+    },
+    { duration: 500, easing: 'ease', fill: 'forwards' }
+  );
+  curSlide.animate(
+    { left: 0 },
+    { duration: 500, easing: 'ease', fill: 'forwards' }
+  );
+  prevSlide.animate(
+    { left: isLeft ? '-100%' : '100%' },
+    { duration: 500, easing: 'ease' }
+  );
 }
 
 function slideAndPaginate() {
